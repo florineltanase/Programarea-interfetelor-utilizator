32e1d6a161ea72f065bc80743cd42dbc6fa259ff
Eu <Eu@DESKTOP-RM5LM19>
2020-03-25 17:10:32 +0200

    Aplicare

---
+++ Proiect_PIU/Proiect_PIU/Masina.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proiect_Piu
+{
+    public class Masina
+    {
+        string marca;
+        string model;
+        string culoare;
+        double pret;
+        int anFabricatie;
+
+        public Masina(string v)
+        {
+            marca = string.Empty;
+            model = string.Empty;
+            culoare = string.Empty;
+            pret = 0;
+            anFabricatie = 0;
+        }
+
+        public Masina(string _marca, string _model, string _culoare, double _pret, int _anFabricatie)
+        {
+            marca = _marca;
+            model = _model;
+            culoare = _culoare;
+            pret = _pret;
+            anFabricatie = _anFabricatie;
+
+        }
+
+        public void Afisare()
+        {
+            Console.WriteLine("Masina este {0}, modelul fiind {1}, are culoarea {2}, pretul de {3} euro , " +
+                "fiind fabricata in {4}", marca, model, culoare, pret, anFabricatie);
+        }
+
+    }
+}
---
+++ Proiect_PIU/Proiect_PIU/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Proiect_Piu
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Masina m1 = new Masina("VW", "Polo", "albastru perlat", 1250, 2006);
+            m1.Afisare();
+            Masina m2 = new Masina("BMW", "X5", "negru", 15000, 2012);
+            m2.Afisare();
+            Masina m3 = new Masina("Dacia", "Logan", "verde", 5000, 2015);
+            m3.Afisare();
+
+
+        }
+    }
+}
