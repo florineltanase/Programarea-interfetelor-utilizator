bcd59fa89f861602792e2998fe626672c21712c4
Eu <Eu@DESKTOP-RM5LM19>
2020-03-31 23:07:13 +0300

    laborator

---
+++ EvidentaStudenti/EvidentaStudenti/Program.cs
@@ -0,0 +1,82 @@
+﻿using LibrarieEntitati;
+using System;
+
+
+namespace ManagementStudenti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student();
+            bool ok = true;
+            string opt;
+            while (ok)
+            {
+                Console.Clear();
+                Console.WriteLine("MENIU\n");
+                Console.WriteLine("S.Citeste note de la tastatura ca un sir de caractere\n");
+                Console.WriteLine("T.Citeste note de la tastatura (cate o nota pe rand)\n");
+                Console.WriteLine("C.Citeste note din linia de comanda\n");
+                Console.WriteLine("A.Afisare note\n");
+                Console.WriteLine("I.Info autor\n");
+                Console.WriteLine("X.Iesire\n");
+                Console.Write("\r\nAlegeti o optiune: ");
+
+                opt = Console.ReadLine();
+                opt = opt.ToUpper();
+                switch (opt)
+                {
+                    case "S":
+                        Console.WriteLine("Introduceti notele separate prin <,>");
+                        s.SetNote(Console.ReadLine());
+                        Console.ReadKey();
+                        break;
+                    case "T":
+                        int an;
+                        Console.WriteLine("Dati numarul de ani inregistrati cu note");
+                        an = Int32.Parse(Console.ReadLine());
+                        int[][] notes = new int[4][];
+                        for (int i = 0; i < 4; i++)
+                        {
+                            notes[i] = new int[15];
+                        }
+                        for (int i = 0; i < an; i++)
+                        {
+                            Console.WriteLine("Dati numarul de note din anul {0}", i);
+                            int note1 = Int32.Parse(Console.ReadLine());
+                            for (int j = 0; j < note1; j++)
+                            {
+                                Console.WriteLine("Introduceti nota nr{0}", j);
+                                int x = Convert.ToInt32(Console.ReadLine());
+                                notes[i][j] = x;
+                            }
+                        }
+                        s.note = notes;
+                        Console.ReadKey();
+                        break;
+                    case "C":
+                        s.SetNote(String.Join(" ", args));
+
+                        Console.ReadKey();
+                        break;
+                    case "A":
+                        Console.WriteLine(s.ConversieLaSir());
+                        Console.ReadKey();
+                        break;
+                    case "I":
+                        Console.WriteLine("Florinel Alexandru Tanase\nGrupa 3123a\n");
+
+                        Console.ReadKey();
+                        break;
+
+                    case "X":
+                        ok = false;
+                        Console.ReadKey();
+                        break;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ EvidentaStudenti/LibrarieEntitati/Student.cs
@@ -0,0 +1,97 @@
+﻿using System;
+namespace LibrarieEntitati
+{
+    public class Student
+    {
+        // date membre private
+        public string nume { set; get; }
+        public int[][] note { set; get; }
+
+        // contructor implicit
+        public Student()
+        {
+        }
+
+        // constructor cu parametri
+        public Student(string _nume)
+        {
+            nume = _nume;
+            note = new int[4][];
+            for (int i = 0; i < 4; i++)
+            {
+                note[i] = new int[15];
+            }
+
+
+        }
+        public void SetNote(int[][] notes, int[][] notess)
+        {
+            notes = notess;
+
+        }
+
+        public void SetNote(string sirNote)
+        {
+            note = new int[4][];
+            for (int i = 0; i < 4; i++)
+            {
+                note[i] = new int[15];
+            }
+
+            if (!sirNote.Equals(""))
+            {
+                string[] buff1 = sirNote.Split(',');
+                // diml = buff1.Length;
+                for (int i = 0; i < buff1.Length; i++)
+                {
+                    string[] buff2 = buff1[i].Split(' ');
+
+                    for (int j = 0; j < buff2.Length; j++)
+                    {
+                        int number;
+                        bool succes = Int32.TryParse(buff2[j], out number);
+                        if (succes)
+                        {
+                            note[i][j] = number;
+                        }
+
+                    }
+                }
+            }
+
+        }
+
+        public string ConversieLaSir()
+        {
+            string sNote = "Nu exista (Nu ati apelat metoda **setNote**)";
+            if (note == null)
+            {
+                return sNote;
+            }
+            else
+            {
+                sNote = String.Empty;
+                for (int i = 0; i < 4; i++)
+                {
+
+                    sNote = String.Concat(sNote, "\nNotele pentru anul nr.", string.Format("{0} sunt:\n", i + 1));
+
+                    for (int j = 0; j < 10; j++)
+                    {
+
+                        if (note[i][j] != 0)
+                        {
+                            sNote = String.Concat(sNote, string.Format("nota este: {0} \n", note[i][j]));
+                        }
+
+                    }
+                }
+
+
+
+            }
+            return sNote;
+        }
+
+    }
+}
